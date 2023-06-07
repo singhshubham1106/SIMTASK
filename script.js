d92866c1fcb72329a73011c062cab82428f6ae82
Shubham Kumar <103987380+singhshubham1106@users.noreply.github.com>
2023-06-07 15:57:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+async function ShortenURL() {
+    const url = document.getElementById("url").value;
+    const response = await fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
+    if (response.ok) {
+      const data = await response.text();
+      document.getElementById('result').innerHTML = `
+        Shortened URL: <a href="${data}" target="_blank">${data}</a>
+      `;
+    } else {
+      document.getElementById('result').innerHTML = "Error Shortening URL";
+    }
+  }
